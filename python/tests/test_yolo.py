6e6b5ca9e951800787609a08226956e037fd7177
MATHIS LAMBERT <mathis.lambert27@gmail.com>
2024-01-15 20:09:35 +0100

    [+] renamed yolo class methods

--- python/tests/test_yolo.py
+++ python/tests/test_yolo.py
@@ -10,14 +10,14 @@ def yolo():
 
 def test_run_detection(yolo):
     frame = cv2.imread("./images/test.jpeg")
-    results, f = yolo.runDetection(frame)
+    results, f = yolo.run_detection(frame)
     assert len(results) > 0
     assert f is not None
 
 
 def test_parse_results(yolo):
     results = yolo.model(cv2.imread("./images/test.jpeg"))
-    peoples = yolo.parseResults(results)
+    peoples = yolo.parse_results(results)
     assert isinstance(peoples, list)
     assert len(peoples) > 0
     assert isinstance(peoples[0], dict)
@@ -25,15 +25,15 @@ def test_parse_results(yolo):
 
 def test_get_json_data(yolo):
     data = {"test": "data"}
-    json_data = yolo.getJsonData(data)
+    json_data = yolo.get_json_data(data)
     assert isinstance(json_data, str)
     assert json_data == '{"test": "data"}'
 
 
 def test_get_peoples(yolo):
     results = yolo.model(cv2.imread("./images/test.jpeg"))
-    yolo.parseResults(results)
-    peoples = yolo.getPeoples()
+    yolo.parse_results(results)
+    peoples = yolo.get_peoples()
     assert isinstance(peoples, list)
     assert len(peoples) > 0
 
